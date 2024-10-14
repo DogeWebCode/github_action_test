67aaffcb2b0f663a830b32add3b8930e9b08f659
DogeWebCode <james56012623@gmail.com>
2024-10-14 14:32:24 +0800

    step1

--- rental_backend/src/main/java/tw/school/rental_backend/config/AwsS3Config.java
+++ rental_backend/src/main/java/tw/school/rental_backend/config/AwsS3Config.java
@@ -1,7 +1,6 @@
 package tw.school.rental_backend.config;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -12,21 +11,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AwsS3Config {
 
-    @Value("${cloud.aws.credentials.accessKey}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secretKey}")
-    private String secretKey;
-
     @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
     public AmazonS3 amazonS3() {
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
         return AmazonS3ClientBuilder.standard()
                 .withRegion(Regions.fromName(region))
-                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
                 .build();
     }
 }
